68f8d5be342136c3ff84d504baf4d30cc69e804d
Andres Campuzano <campuxano88@gmail.com>
2020-05-29 11:56:04 -0500

    Stories, Header, Footer, SideBar, TimeLine.

---
+++ src/components/Profile.svelte
@@ -0,0 +1,11 @@
+<div class="Profile">
+  <div class="Profile-content">
+    <div class="Profile-avatar">
+      <img src="https://pbs.twimg.com/profile_images/1197212422194520065/eoys9Yjd_400x400.jpg" alt="avatar profile" />
+    </div>
+    <div class="Profile-info">
+      <h2>AndresCampuzano</h2>
+      <span>Andres Campuzano</span>
+    </div>
+  </div>
+</div>
---
+++ src/components/Stories.svelte
@@ -0,0 +1,16 @@
+<div class="Stories">
+  <div class="Stories-container">
+    <div class="Stories-head">
+      <h2>Stories</h2>
+      <span>Watch all</span>
+    </div>
+    <div class="Stories-items">
+      <div class="Stories-item">
+        <div class="Stories-item-box">
+          <img src="https://pbs.twimg.com/profile_images/1197212422194520065/eoys9Yjd_400x400.jpg" alt="avatar username" />
+        </div>
+        <h2>campuzanodev <span>10 hours before</span></h2>
+      </div>
+    </div>
+  </div>
+</div>
