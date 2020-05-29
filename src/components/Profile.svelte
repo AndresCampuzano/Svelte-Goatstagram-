86ef1f645f999643cc63b72a0f99b2589f2eac13
Andres Campuzano <campuxano88@gmail.com>
2020-05-29 12:43:23 -0500

    Styles.

--- src/components/Profile.svelte
+++ src/components/Profile.svelte
@@ -1,10 +1,35 @@
+<style>
+  .Profile-content {
+    display: flex;
+    align-items: center;
+  }
+  .Profile-avatar img {
+    width: 42px;
+    height: 42px;
+    border-radius: 50%;
+  }
+  .Profile-info {
+    margin: 0 0 0 0.5em;
+  }
+  .Profile-info h2 {
+    font-size: 14px;
+    color: black;
+    margin: 0;
+    padding: 0;
+  }
+  .Profile-info span {
+    font-size: 12px;
+    font-weight: normal;
+  }
+</style>
+
 <div class="Profile">
   <div class="Profile-content">
     <div class="Profile-avatar">
       <img src="https://pbs.twimg.com/profile_images/1197212422194520065/eoys9Yjd_400x400.jpg" alt="avatar profile" />
     </div>
     <div class="Profile-info">
-      <h2>AndresCampuzano</h2>
+      <h2>campuzanodev</h2>
       <span>Andres Campuzano</span>
     </div>
   </div>
--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -4,6 +4,15 @@
   import Footer from "./Footer.svelte";
 </script>
 
+<style>
+  .Sidebar {
+    position: relative;
+    padding: 4.5em 0 0 0;
+  }
+  .Sidebar-container {
+    position: fixed;
+  }
+</style>
 
 <div class="Sidebar">
   <div class="Sidebar-container">
